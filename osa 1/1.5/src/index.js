c8e18092c54d892caab95549c8a6de33035977a1
Ville <veill@luukku.com>
2018-11-10 18:36:23 +0200

    tehtävä 1.5

---
+++ osa 1/1.5/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    const Osa = (props) => {
+        return (
+            <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+        )
+    }
+
+    return (
+        <div>
+          <Osa osa={props.osat[0]} />
+          <Osa osa={props.osat[1]} />
+          <Osa osa={props.osat[2]} />
+        </div>
+      )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
+    )
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa osat={kurssi.osat} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
